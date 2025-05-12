42b21a8779a09af32fe6e698de4d5f0162e1e29a
qiewi <105619444+qiewi@users.noreply.github.com>
2025-05-12 19:51:41 +0700

    feat: Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,21 +1,21 @@
 import Link from "next/link"
-import { Book, Home, User } from "lucide-react"
+import { FaBookOpen, FaHome, FaUserCircle } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 py-2">
+    <nav className="fixed bottom-0 left-0 right-0 bg-white rounded-t-3xl border py-2 shadow-[0_-8px_24px_rgba(0,0,0,0.1)]">
       <div className="flex justify-around items-center">
         <Link href="/ebook" className="flex flex-col items-center p-2">
-          <Book className="h-6 w-6 text-gray-500" />
-          <span className="text-xs text-gray-500 mt-1">E-book</span>
+          <FaBookOpen className="h-6 w-6 text-gray-500" />
+          <span className="text-xs text-gray-500 mt-1">E-Book</span>
         </Link>
         <Link href="/home" className="flex flex-col items-center p-2">
-          <Home className="h-6 w-6 text-gray-900" />
-          <span className="text-xs text-gray-900 mt-1">Beranda</span>
+          <FaHome className="h-6 w-6 text-gray-900" />
+          <span className="text-xs text-gray-900 mt-1">Home</span>
         </Link>
         <Link href="/profile" className="flex flex-col items-center p-2">
-          <User className="h-6 w-6 text-gray-500" />
-          <span className="text-xs text-gray-500 mt-1">Profil</span>
+          <FaUserCircle className="h-6 w-6 text-gray-500" />
+          <span className="text-xs text-gray-500 mt-1">Profile</span>
         </Link>
       </div>
     </nav>
